0353e3ed2cb0eadfa18a911b332dd06ef2a67bbe
pattoche <30961098+pattoche@users.noreply.github.com>
2020-08-31 17:46:45 +0200

    commit de surete

--- Backoffice/rando/src/main/java/com/rando/dao/ItineraireDao.java
+++ Backoffice/rando/src/main/java/com/rando/dao/ItineraireDao.java
@@ -15,36 +15,34 @@ import com.rando.modele.Itineraire;
 public class ItineraireDao {
 
 	@PersistenceContext
-	private EntityManager em;	
+	private EntityManager em;
 
 	public boolean existe(String nomIteneraire) {
 		long nb = em.createQuery("select count(i) from Iteneraire i where lower(i.nom) = lower(:nom)", Long.class)
 				.setParameter("nom", nomIteneraire).getSingleResult();
 		return nb > 0;
 	}
-	
+
 	public void ajouterItineraire(Itineraire itineraire, Etapeitineraire etapeitineraire) {
 		em.persist(itineraire);
 		em.persist(etapeitineraire);
 	}
-	
+
 	public Itineraire getIteneraire(int itineraireId) {
 		return em.find(Itineraire.class, itineraireId);
 	}
-	
+
 	public List<Itineraire> getAllIteneraires() {
 		return em.createQuery("select i from Iteneraire i order by i.nom", Itineraire.class).getResultList();
 	}
 
 	public void modifierItineraire(long itineraireId, ItineraireDto itineraireDto) {
-		em.createQuery(
-				"update Itineraire i set i.nom=:nom, i.niveau=:niveau where i.id=:id")
-				.setParameter("nom", itineraireDto.getNom())
-				.setParameter("niveau", itineraireDto.getNiveau())
+		em.createQuery("update Itineraire i set i.nom=:nom, i.niveau=:niveau where i.id=:id")
+				.setParameter("nom", itineraireDto.getNom()).setParameter("niveau", itineraireDto.getNiveau())
 				.setParameter("id", itineraireId).executeUpdate();
 	}
-	
-	public void supprimerItineraire(int itineraireId) {	
-		em.createQuery("delete from Iteneraire i where i.nom = :nom").setParameter("nom", itineraireId).executeUpdate();
+
+	public void supprimerItineraire(int iteneraireId) {
+		em.createQuery("delete from Iteneraire i where i.id = :id").setParameter("id", iteneraireId).executeUpdate();
 	}
 }
