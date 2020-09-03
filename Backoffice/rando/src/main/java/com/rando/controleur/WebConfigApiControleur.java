1d1b798e1fb649181600e235f398e3dd22e475fc
pattoche <30961098+pattoche@users.noreply.github.com>
2020-09-03 22:01:50 +0200

    create feature web_api

---
+++ Backoffice/rando/src/main/java/com/rando/controleur/WebConfigApiControleur.java
@@ -0,0 +1,34 @@
+package com.rando.controleur;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rando.modele.Itineraire;
+import com.rando.modele.Utilisateur;
+import com.rando.service.ItineraireService;
+import com.rando.service.UtilisateurService;
+
+@RestController
+@RequestMapping("/api")
+public class WebConfigApiControleur {
+	@Autowired
+	private UtilisateurService utilisateurService;
+	@Autowired
+	private ItineraireService itineraireService;
+
+    @GetMapping(path="/utilisateurs", produces= "application/json")
+    public List<Utilisateur> getAllUsers() {
+    	List<Utilisateur> users = utilisateurService.getAllUsers();
+        return users;
+    }
+    
+    @GetMapping(path="/itineraires", produces= "application/json")
+    public List<Itineraire> getAllItineraires() {
+    	List<Itineraire> itineraires = itineraireService.getItineraires();
+        return itineraires;
+    }
+}
